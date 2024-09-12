d5002313f58cd6f9f9ae2ce49b94e9485fc90e45
wyoung1810 <ukjoma@gmail.com>
2024-09-12 16:10:31 -0400

    Completed functionality of square driving node and added comments

--- warmup_project/square.py
+++ warmup_project/square.py
@@ -2,36 +2,45 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Header, ColorRGBA
-from visualization_msgs.msg import Marker
-from geometry_msgs.msg import Twist, Vector3
+from geometry_msgs.msg import Twist
 
-class SendMessageNode(Node):
-    """This is a mesasge sending node which inherits from Node."""
+class DriveSquareNode(Node):
+    """This is a node for driving the neato in a 1m by 1m square. Inherits from Node."""
     def __init__(self):
-        super().__init__("send_message_node")
+        super().__init__("drive_square_node")
         #Create timer that calls the loop every 0.1 seconds
         timer_period = 0.1
         self.timer = self.create_timer(timer_period, self.run_loop)
         #Create counter to track time
         self.counter = 0
-        #Create publisher
-        self.publisher = self.create_publisher(Twist, "neato_square", 10)
+        #Create publisher to neato's velocity topic
+        self.publisher = self.create_publisher(Twist, "cmd_vel", 10)
 
     def run_loop(self):
         "Drives the neato in a square."
-        # Increment the timer
+        # initialize a velocity message for the neato
         cmd_vel = Twist()
-        # it takes 1 second for the neato to go 1 m so go for 10 increments
-        if (self.counter % 30 < 10):
-            cmd_vel.linear.x = 1.0
+        # it takes 5 seconds for the neato to go 1 m so go for 50 increments
+        if (self.counter % 100 < 50):
+            cmd_vel.linear.x = 0.2
             cmd_vel.angular.z = 0.0
-        else:
-            # Set velocity to pi/4 so it will turn 90 degrees in exactly 2 seconds
+        # wait for a second to allow the neato to stop fully
+        elif (self.counter % 100 < 60):
             cmd_vel.linear.x = 0.0
-            cmd_vel.angular.z = .7854
+            cmd_vel.angular.z = 0.0
+        elif (self.counter % 100 < 90):
+            # Set velocity to pi/6 so it will turn 90 degrees in exactly 3 seconds
+            cmd_vel.linear.x = 0.0
+            cmd_vel.angular.z = .5236
+        # wait for a second to allow the neato to stop fully
+        else: 
+            cmd_vel.linear.x = 0.0
+            cmd_vel.angular.z = 0.0
+
+        # increment counter
         self.counter += 1
-        # my_header = Header(stamp=self.get_clock().now().to_msg(), frame_id = "odom")
+
+        # publish message
         print(cmd_vel)
         print(self.counter)
         self.publisher.publish(cmd_vel)
@@ -39,7 +48,7 @@ class SendMessageNode(Node):
 def main(args=None):
     """Initialize our node, run it, and cleanup on shutdown."""
     rclpy.init(args=args)
-    node = SendMessageNode()
+    node = DriveSquareNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
