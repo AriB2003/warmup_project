ac36816e678cd3424f71eb789dc420a4ca1eb27e
Ari Bobesh <ari.bobesh@gmail.com>
2024-09-16 12:23:22 -0400

    Got person_follower working in simulation.

--- warmup_project/person_follower.py
+++ warmup_project/person_follower.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist, Vector3
 import math
        
@@ -14,9 +15,17 @@ class PersonFollowerNode(Node):
         self.scan_results = None
         self.timer = self.create_timer(timer_period, self.run_loop)
         self.sub = self.create_subscription(LaserScan, 'scan', self.process_scan, 10)
+        self.sub2 = self.create_subscription(Odometry, 'odom', self.process_odom, 10)
         self.publisher = self.create_publisher(Twist, "cmd_vel", 10)
         self.last_direction = [0]
         self.front_scan = None
+        self.last_heading = 0
+        self.current_heading = 0
+        self.angular = 0
+
+    def process_odom(self, msg):
+        self.current_heading = 360*msg.pose.pose.orientation.z
+        # print(self.current_heading)
 
     def process_scan(self, msg):
         self.scan_results = [4-v if not math.isinf(v) else 0 for v in msg.ranges]
@@ -28,8 +37,11 @@ class PersonFollowerNode(Node):
         #Ensures a scan has been completed before writing data
         if (self.scan_results != None):
             direction = self.detect_person()
+            hd = round(self.angular*180/math.pi/10)
+            self.last_direction = [abs((d+hd)) for d in self.last_direction[-4:]]
             self.last_direction.append(direction)
-            self.last_direction = self.last_direction[-5:]
+            self.last_heading = self.current_heading
+            # print(self.last_direction)
             # print(direction)
             # Determine if wall is on right or left side of neato
             # min_scan_dist = 99999999999
@@ -42,15 +54,21 @@ class PersonFollowerNode(Node):
             # Calculate the angle between neato heading and wall direction
         
             # Publish correction to neato by changing angular velocity
-            speed = [i/2*(180-d)/180 for i,d in enumerate(self.last_direction)]
-            # print(speed)
+            # speed = [i/5*(180-d)/180 for i,d in enumerate(self.last_direction)]
+            if sum(self.last_direction)/5>180:
+                speed = [-(180-abs(d-180))/180*5 for i,d in enumerate(self.last_direction)]
+            else:
+                speed = [(180-abs(d-180))/180*5 for i,d in enumerate(self.last_direction)]
+            # print(sum(speed)/5)
             msg = Twist()
-            # msg.linear.x = 0.2
-            msg.angular.z = sum(speed)/5
-            forward = sum(self.front_scan)/len(self.front_scan)-0.5
-            print(self.front_scan)
-            if abs(sum(speed)/5)<0.35:
+            # self.angular = 0.35
+            self.angular = sum(speed)/5
+            msg.angular.z = self.angular
+            forward = sum(self.front_scan)/len(self.front_scan)-0.25
+            # print(self.front_scan)
+            if abs(sum(speed)/5)<0.7:
                 msg.linear.x = forward
+                pass
             else:
                 msg.linear.x = 0.0
             self.publisher.publish(msg)
@@ -66,12 +84,13 @@ class PersonFollowerNode(Node):
         # print(self.last_direction)
         for i, d in enumerate(self.last_direction):
             self.scan_results[min(d, len(self.scan_results)-1)] = 2+i
-        kernel = [-8,-7,-6,-5,-4,-3,-2,-1,-1,1,1,1,2,4,5,4,2,1,1,1,-1,-1,-2,-3,-4,-5,-6,-7,-8]
+        kernel = [-4,-2,-2,-2,-2,-2,-1,-1,-1,1,2,2,4,6,8,6,4,2,2,1,-1,-1,-1,-2,-2,-2,-2,-2,-4]
         o = len(kernel)//2
         self.scan_results = self.scan_results[-o:]+self.scan_results+self.scan_results[:o]
         convolution = []
         for i in range(o,len(self.scan_results)-o):
             convolution.append(sum([k*v for k,v in zip(kernel, self.scan_results[i-o:i+o])]))
+        # print(convolution)
         return convolution.index(max(convolution))
         
 
